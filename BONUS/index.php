0a33b96b004b653d5a9a6dd7f78d9797d9a7c6df
albertopiffer <albertopiffer96@gmail.com>
2023-03-30 16:05:27 +0200

    FEAT bonus1

--- BONUS/index.php
+++ BONUS/index.php
@@ -12,12 +12,12 @@ class Movie {
     }
 
     public function stampa() {
-        echo $this->titolo . "- genere: " . $this->genere . "- lingua: " . $this->lingua . "<br>";
+        echo $this->titolo . " - genere: " . implode(", ", $this->genere) . " - lingua: " . $this->lingua . "<br>";
     }
 }
 
-$f1 = new Movie("The Banshees of Inisherin", "drammatico", "inglese");
-$f2 = new Movie("Everything Everywhere All at Once", "commedia", "inglese");
+$f1 = new Movie("The Banshees of Inisherin", ["drammatico", "commedia"], "inglese");
+$f2 = new Movie("Everything Everywhere All at Once", ["commedia"], "inglese");
 
 $f1->stampa();
 $f2->stampa();
